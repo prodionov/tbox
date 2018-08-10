77f73791a4464a459cd1d9690fc31cf81278eb97
prodionov <pavelrodionov@hotmail.com>
2018-08-10 11:41:31 +0100

    added signup button and redirection to the app.js Reference #1

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
 import React, { Component } from "react";
-import LandingPage from "./components/landing";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import LandingPage from "./components/Landing";
+import Signup from "./components/Signup";
 
 class App extends Component {
   render() {
-    return <LandingPage />;
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" render={() => <LandingPage />} />;
+          <Route exact path="/signup" render={() => <Signup />} />;
+        </Switch>
+      </BrowserRouter>
+    );
   }
 }
 
---
+++ client/src/components/Landing.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import { Redirect } from "react-router";
+
+export default class LandingPage extends Component {
+  state = {
+    username: "",
+    password: "",
+    toSignup: false
+  };
+
+  handleChange = event => {
+    const value = event.target.value;
+    const name = event.target.name;
+    this.setState({ [name]: value });
+    console.log(this.state);
+  };
+
+  redirectSignup = event => {
+    console.log("I was clicked");
+    this.setState({ toSignup: true });
+  };
+
+  submitRequest = event => {
+    event.preventDefault();
+    let params = {
+      username: this.state.username,
+      password: this.state.password
+    };
+  };
+
+  render() {
+    if (this.state.toSignup) {
+      return <Redirect to="/signup" />;
+    }
+    return (
+      <div>
+        <h1>Hackathon</h1>
+        <form>
+          <input type="text" id="user" placeholder="Username" />
+          <input type="password" name="pwd" placeholder="Password" />
+          <button onClick={this.submitRequest}>Login</button>
+        </form>
+        <h2>New to the hackathon?</h2>
+        <button onClick={this.redirectSignup}>Sign up</button>
+      </div>
+    );
+  }
+}
