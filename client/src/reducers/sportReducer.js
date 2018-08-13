823a8ad53b79e9bb95b33aba2bb11593cb03f2ed
prodionov <pavelrodionov@hotmail.com>
2018-08-13 09:45:15 +0100

    implemented Redux to store login details in the local storage Reference #23

---
+++ client/src/reducers/sportReducer.js
@@ -0,0 +1,44 @@
+import { addUser } from "../utils/addUser";
+const SELECT_TEAM = "SELECT_TEAM";
+
+export const selectTeamAction = team => {
+  console.log("team", team);
+  return function(dispatch) {
+    addUser("./winners", team).then(response => {
+      console.log("response", response);
+      if (response.result !== "failure") {
+        let games = response.result.rows;
+        let defeatedTeams = games.reduce((acc, game) => {
+          if (team.team !== game.home_team) {
+          }
+          if (team.team !== game.home_team) {
+            acc.push([game.game_date, game.home_team]);
+          } else {
+            acc.push([game.game_date, game.away_team]);
+          }
+          return acc;
+        }, []);
+        console.log("almost there", defeatedTeams);
+        dispatch({
+          type: SELECT_TEAM,
+          payload: defeatedTeams
+        });
+      }
+    });
+  };
+};
+
+let initialState = [];
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case SELECT_TEAM:
+      return [
+        {
+          teams: action.payload
+        }
+      ];
+    default:
+      return state;
+  }
+}
