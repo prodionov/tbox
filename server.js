b87bcb817d462b019e668c2fc12f0df177515075
prodionov <pavelrodionov@hotmail.com>
2018-08-13 11:06:02 +0100

    modified server so it can handle static files now

--- server.js
+++ server.js
@@ -13,6 +13,12 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(express.static(path.join(__dirname, "client/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
+
 app.post("/register", addUser);
 app.post("/login", loginUser);
 app.post("/winners", teamWins);
