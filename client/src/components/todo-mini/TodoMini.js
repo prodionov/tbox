0f4b0dccf908fe3075e3034f2cf425bcd45605e8
prodionov <pavelrodionov@hotmail.com>
2018-08-13 09:35:19 +0100

    added todo and todo-mini components Reference#18

---
+++ client/src/components/todo-mini/TodoMini.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import("./style.css");
+
+class TodoMini extends Component {
+  render() {
+    const tasks = this.props.todoList;
+    console.log("tasks", tasks);
+    return tasks.length === 0 ? (
+      <h2 className="nothing-message">No tasks to display</h2>
+    ) : (
+      <ul>
+        {tasks.map((task, index) => {
+          return (
+            <li className="Task-item" key={index}>
+              <div className="Task-container">{task.task}</div>
+            </li>
+          );
+        })}
+      </ul>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    isLoggedin: state.login.isLoggedin,
+    todoList: state.todo
+  };
+};
+
+export default connect(mapStateToProps, {})(TodoMini);
---
+++ client/src/components/todo/Todo.js
@@ -0,0 +1,97 @@
+import React, { Component } from "react";
+import { addTask, completedTask } from "../../reducers/todoReducer";
+import "./style.css";
+import { connect } from "react-redux";
+import Login from "../login/Login";
+
+const Task = result => {
+  console.log(result);
+  return (
+    <div className="Task-container">
+      <li>{result.task}</li>
+      <input type="checkbox" />
+    </div>
+  );
+};
+
+class Todo extends Component {
+  state = {
+    task: "",
+    todoList: []
+  };
+
+  handleChange = event => {
+    const value = event.target.value;
+    const name = event.target.name;
+    this.setState({ [name]: value });
+  };
+
+  handleCheck = task => {
+    console.log("task", task);
+    this.props.completedTask(task.id);
+  };
+
+  addTask = event => {
+    event.preventDefault();
+    let task = this.state.task;
+    task ? this.props.addTask(task) : alert("You cannot add an empty task");
+    this.setState({ task: "" });
+  };
+
+  render() {
+    if (!this.props.isLoggedin) {
+      return <Login />;
+    }
+    let list = this.props.todoList.slice();
+    list.reverse();
+    return (
+      <div className="todo-page">
+        <div className="todo-headline">
+          <h1>ToDo List</h1>
+        </div>
+        <div className="todo-form">
+          <form>
+            <input
+              className="form-input"
+              type="text"
+              name="task"
+              id="task"
+              value={this.state.task}
+              onChange={this.handleChange}
+              placeholder="Add task here"
+            />
+            <button onClick={this.addTask} className="todo-button" />
+          </form>
+        </div>
+        <div className="todo-list">
+          <ul>
+            {list.map((task, index) => {
+              return (
+                <li key={index}>
+                  <div className="Task-container">
+                    <span className="task-item">{task.task}</span>
+                    <input
+                      type="checkbox"
+                      checked={task.checked}
+                      onChange={() => this.handleCheck(task)}
+                    />
+                  </div>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    isLoggedin: state.login.isLoggedin,
+    username: state.login.username,
+    todoList: state.todo
+  };
+};
+
+export default connect(mapStateToProps, { addTask, completedTask })(Todo);
